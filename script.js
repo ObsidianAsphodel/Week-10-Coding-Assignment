9a561304cef3972108b9668223c552e233e1c78b
Terence Dixon <terencep.dixon@gmail.com>
2022-08-05 02:47:48 -0700

    added tables, forms and js

---
+++ script.js
@@ -0,0 +1,6 @@
+let firstName = document.getElementById("first-name");
+console.log(firstName);
+let lastName = document.getElementById("last-name");
+console.log(lastName);
+let Handle = document.getElementById("handle-name");
+console.log(Handle);
