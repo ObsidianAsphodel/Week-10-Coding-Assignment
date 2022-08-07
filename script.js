eacf380f6da4e5d30048e90a38209818b922196b
Terence Dixon <terencep.dixon@gmail.com>
2022-08-06 22:01:56 -0700

    added functionality to create new table rows

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let firstName = document.getElementById("first-name");
-console.log(firstName);
-let lastName = document.getElementById("last-name");
-console.log(lastName);
-let Handle = document.getElementById("handle-name");
-console.log(Handle);
+console.log(document.getElementById("add"));
+console.log(document.getElementById('list'));
+
+document.getElementById("add").addEventListener("click", () => {
+    let table = document.getElementById('list');
+    let row = table.insertRow(1);
+    row.setAttribute('id', `item-${id}`);
+    row.insertCell(0).innerHTML = document.getElementById("first-name").value;
+    console.log(document.getElementById("first-name").value);
+    row.insertCell(1).innnerHTML = document.getElementById("last-name").value;
+    console.log(document.getElementById("last-name").value);
+});
